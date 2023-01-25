d06d4f2ae81ffc8980134108208a444f3a582f59
Anwesh Mishra <19btcse07@suiit.ac.in>
2023-01-25 15:49:56 +0530

    feats : Chats added

---
+++ src/components/Chats.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Chats = () => {
+  return (
+    <div className='chats'>
+      <div className="userChat">
+         <img src="https://images.pexels.com/photos/678783/pexels-photo-678783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+         <div className="userChatInfo">
+            <span>Jane</span>
+            <p>Hello</p>
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default Chats
