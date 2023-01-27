9a8fdcf1f9e9f6a60f0a8bb031eeb1e944a4b4ba
Anwesh Mishra <19btcse07@suiit.ac.in>
2023-01-27 12:13:56 +0530

    feat : search user added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { db } from '../firebase'
+import { collection, getDocs, query, where } from 'firebase/firestore'
 
 const Search = () => {
-  return (
-    <div className='search'>
-      <div className="searchForm">
-         <input type="text" placeholder='Search' />
-      </div>
-      <div className="userChat">
-         <img src="https://images.pexels.com/photos/678783/pexels-photo-678783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-         <div className="userChatInfo">
-            <span>Jane</span>
+   const [username, setUsername] = useState("")
+   const [user, setUser] = useState(null)
+   const [err, setErr] = useState(false)
+   const handleSearch = async () => {
+      const q = query(
+         collection(db, "user"),
+         where("username", "==", username)
+      )
+      try {
+         const querySnapshot = await getDocs(q);
+         querySnapshot.forEach((doc) => {
+            setUser(doc.data());
+         });
+      } catch (err) {
+         setErr(true)
+      }
+   }
+   const handleKey = (e) => {
+      e.code === "Enter" && handleSearch()
+   }
+   const handleSelect = () => {
+
+   }
+   return (
+      <div className='search'>
+         <div className="searchForm">
+            <input type="text" placeholder='Search' onKeyDown={handleKey} onChange={e => setUsername(e.target.value)} />
          </div>
+         {err && <span>User not found!</span>}
+         {user && (
+            <div className="userChat" onClick={handleSelect}>
+               <img src={user.photoURL} alt="" />
+               <div className="userChatInfo">
+                  <span>{user.username}</span>
+               </div>
+            </div>
+         )}
       </div>
-    </div>
-  )
+   )
 }
 
 export default Search
