1e0a1b37eb03d3ea2d487bf4c108520497e34e5d
Anwesh Mishra <19btcse07@suiit.ac.in>
2023-01-28 01:52:44 +0530

    feat : scroll to latest message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,19 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
+
 import { AuthContext } from '../context/AuthContext'
 import { ChatContext } from '../context/ChatContext'
 
 const Message = ({ message }) => {
-  const {currentUser} = useContext(AuthContext)
-  const {data} = useContext(ChatContext)
+  const { currentUser } = useContext(AuthContext)
+  const { data } = useContext(ChatContext)
+  const ref = useRef();
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
   return (
-    <div className='message owner'>
+    <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
       <div className="messageInfo">
-        <img src="" alt="" />
+        <img src={
+          message.senderId === currentUser.uid
+            ? currentUser.photoURL
+            : data.user.photoURL
+        } alt="" />
         <span>Just Now</span>
       </div>
       <div className="messageContent">
-        <p>Hello</p>
-        <img src="https://images.pexels.com/photos/678783/pexels-photo-678783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
       </div>
     </div>
   )
