4ec534626318625273e2d2f456b93c73574d25aa
Anwesh Mishra <19btcse07@suiit.ac.in>
2023-01-27 08:41:42 +0530

    feat : signout added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
+import { signOut } from 'firebase/auth'
 import React from 'react'
+import { auth } from '../firebase'
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext)
   return (
     <div className='navbar'>
       <span className="logo">Tele Chat</span>
       <div className='user'>
-         <img src="https://images.pexels.com/photos/678783/pexels-photo-678783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-         <span>Anwesh Mishra</span>
-         <button>Logout</button>
+        <img src="https://images.pexels.com/photos/678783/pexels-photo-678783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <span>Anwesh Mishra</span>
+        <button onClick={() => signOut(auth)}>Logout</button>
       </div>
     </div>
   )
