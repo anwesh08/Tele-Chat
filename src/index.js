303634fd59c6775ac56b39fdd0f27a0133ce8302
Anwesh Mishra <19btcse07@suiit.ac.in>
2023-01-27 21:21:38 +0530

    chore : ChatContext

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import ReactDOM from "react-dom/client";
 import { AuthContextProvider } from "./context/AuthContext";
 
 import App from "./App";
+import { ChatContextProvider } from "./context/ChatContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ChatContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChatContextProvider>
   </AuthContextProvider>
 );
